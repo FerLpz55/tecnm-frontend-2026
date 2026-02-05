d475348d27ee6ff9b0115e02d675a09daf3ba107
marcoriv456 <marcosnicolasrivera456@gmail.com>
2026-02-05 10:06:24 -0600

    feat: Add scroll effects to register page

--- src/app/ui/pages/register/register.page.ts
+++ src/app/ui/pages/register/register.page.ts
@@ -4,11 +4,12 @@ import { LabeledSubtitle } from '../../atoms/labeled-subtitle/labeled-subtitle';
 import { Panel } from '../../atoms/panel/panel';
 import { Text } from '../../atoms/text/text';
 import { Title } from '../../atoms/title/title';
+import { Intersectable } from '../../directives/intersectable/intersectable';
 import { TeamInfoForm } from './ui/organisms/team-info-form/team-info-form';
 
 @Component({
   selector: 'hack-register',
-  imports: [Title, Text, Button, Panel, LabeledSubtitle, TeamInfoForm],
+  imports: [Title, Text, Button, Panel, LabeledSubtitle, TeamInfoForm, Intersectable],
   templateUrl: './register.page.html',
   styleUrl: './register.page.css',
 })
